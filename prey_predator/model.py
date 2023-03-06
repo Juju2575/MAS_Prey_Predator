a2966ad62ffbf08076cf2ab0172fcfeb2add5340
Juliette Cochez <juliette2575@gmail.com>
2023-03-06 17:46:06 +0100

    Initial commit

---
+++ prey_predator/model.py
@@ -0,0 +1,128 @@
+"""
+Prey-Predator Model
+================================
+
+Replication of the model found in NetLogo:
+    Wilensky, U. (1997). NetLogo Wolf Sheep Predation model.
+    http://ccl.northwestern.edu/netlogo/models/WolfSheepPredation.
+    Center for Connected Learning and Computer-Based Modeling,
+    Northwestern University, Evanston, IL.
+"""
+
+from mesa import Model
+from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
+
+from prey_predator.agents import Sheep, Wolf, GrassPatch
+from prey_predator.schedule import RandomActivationByBreed
+
+
+class WolfSheep(Model):
+    """
+    Wolf-Sheep Predation Model
+    """
+
+    height = 20
+    width = 20
+
+    initial_sheep = 100
+    initial_wolves = 50
+
+    sheep_reproduce = 0.04
+    wolf_reproduce = 0.05
+
+    wolf_gain_from_food = 20
+
+    grass = False
+    grass_regrowth_time = 30
+    sheep_gain_from_food = 4
+
+    description = (
+        "A model for simulating wolf and sheep (predator-prey) ecosystem modelling."
+    )
+
+    def __init__(
+        self,
+        height=20,
+        width=20,
+        initial_sheep=100,
+        initial_wolves=50,
+        sheep_reproduce=0.04,
+        wolf_reproduce=0.05,
+        wolf_gain_from_food=20,
+        grass=False,
+        grass_regrowth_time=30,
+        sheep_gain_from_food=4,
+    ):
+        """
+        Create a new Wolf-Sheep model with the given parameters.
+
+        Args:
+            initial_sheep: Number of sheep to start with
+            initial_wolves: Number of wolves to start with
+            sheep_reproduce: Probability of each sheep reproducing each step
+            wolf_reproduce: Probability of each wolf reproducing each step
+            wolf_gain_from_food: Energy a wolf gains from eating a sheep
+            grass: Whether to have the sheep eat grass for energy
+            grass_regrowth_time: How long it takes for a grass patch to regrow
+                                 once it is eaten
+            sheep_gain_from_food: Energy sheep gain from grass, if enabled.
+        """
+        super().__init__()
+        # Set parameters
+        self.height = height
+        self.width = width
+        self.initial_sheep = initial_sheep
+        self.initial_wolves = initial_wolves
+        self.sheep_reproduce = sheep_reproduce
+        self.wolf_reproduce = wolf_reproduce
+        self.wolf_gain_from_food = wolf_gain_from_food
+        self.grass = grass
+        self.grass_regrowth_time = grass_regrowth_time
+        self.sheep_gain_from_food = sheep_gain_from_food
+
+        self.schedule = RandomActivationByBreed(self)
+        self.grid = MultiGrid(self.height, self.width, torus=True)
+        self.datacollector = DataCollector(
+            {
+                "Wolves": lambda m: m.schedule.get_breed_count(Wolf),
+                "Sheep": lambda m: m.schedule.get_breed_count(Sheep),
+            }
+        )
+
+        # Create sheep:
+        for i in range(self.initial_sheep):
+            x = self.random.randrange(0,self.grid.width-1)
+            y = self.random.randrange(0,self.grid.height-1)
+            a = Sheep(self.next_id(), (x, y), self, moore = True, energy=10)
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            self.grid.place_agent(a, (x, y))
+
+        # # Create wolves
+        for i in range(self.initial_wolves):
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            a = Wolf(self.next_id(), (x, y), self,moore = True, energy=10)
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            self.grid.place_agent(a, (x, y))
+
+        # Create grass patches (un patch d'herbe sur chaque case)
+        for i in range(self.width):
+            for j in range(self.height):
+                a = GrassPatch(self.next_id(), self, WolfSheep, True, self.grass_regrowth_time)
+                self.schedule.add(a)
+                # Add the agent to a random grid cell
+                self.grid.place_agent(a, (i, j))
+
+    def step(self):
+        self.schedule.step()
+
+        # Collect data
+        self.datacollector.collect(self)
+
+    def run_model(self, step_count=200):
+        for i in range(step_count):
+            self.step()
+

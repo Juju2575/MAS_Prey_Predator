6fc497c9c31e7324142aa1201487d8ac7d8043e5
Juliette Cochez <juliette2575@gmail.com>
2023-03-09 15:41:39 +0100

    Ajout noms binôme

--- prey_predator/model.py
+++ prey_predator/model.py
@@ -6,7 +6,12 @@ Replication of the model found in NetLogo:
     Wilensky, U. (1997). NetLogo Wolf Sheep Predation model.
     http://ccl.northwestern.edu/netlogo/models/WolfSheepPredation.
     Center for Connected Learning and Computer-Based Modeling,
-    Northwestern University, Evanston, IL.
+    Northwestern University, Evanston, IL.*
+    
+================================
+
+Binôme : Clément Blanchoz--Rhône et Juliette Cochez
+    
 """
 
 from mesa import Model
